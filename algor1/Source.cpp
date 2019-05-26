437cad428b78d6dba3e873a2587a707787504e83
Евгений Погорелов <skorpion4ik998@gmail.com>
2019-05-26 16:08:23 +0300

    Простой прямой поиск подстроки в строке. ЗАКОНЧЕНО! ГОТОВО!

--- algor1/Source.cpp
+++ algor1/Source.cpp
@@ -10,13 +10,22 @@ int main()
 	getline(cin, str);
 	cout << "Entry sub-string, witch will try to found: ";
 	getline(cin, substr);
+	int found = -1;
 	for (int i = 0; i < str.size() - substr.size(); ++i)
 	{
 		for (int j = 0; j < substr.size(); ++j)
 		{
-
+			if (str[i + j] != substr[j])
+				break;
+			else if (j == substr.size() - 1)
+				found = i;
 		}
+		if (found != -1)
+			break;
 	}
-
+	if (found != -1)
+		cout << "FOUND! ;-D " << found << endl;
+	else
+		cout << "NOT FOUND! :-C" << endl;
 	return 0;
 }
