8aedcdbec334bd2611282a12fc96994669a047cb
Евгений Погорелов <skorpion4ik998@gmail.com>
2019-05-26 16:20:08 +0300

    Начало алгоритма Кнута-Мориса-Прата (КМП) -Вводи с клавиатуры строки и подстроки поиска -заготовка для найденого вхождения (found)

---
+++ Knyta-Morisa-Prata/Source.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string str, substr;
+	cout << "Entry string: ";
+	getline(cin, str);
+	cout << "Entry sub-string, witch will try to found: ";
+	getline(cin, substr);
+	int found = -1;
+	
+	//
+	//אכדמנטעל
+	//
+
+
+	if (found != -1)
+		cout << "FOUND! ;-D " << found << endl;
+	else
+		cout << "NOT FOUND! :-C" << endl;
+	return 0;
+}
